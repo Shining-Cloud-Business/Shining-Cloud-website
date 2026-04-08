b4403b6c53cf5c1327ade044c24ddf212b97497e
Shining-Cloud-Business <hrshiningcloud@gmail.com>
2026-04-08 17:01:46 +0530

    fix: add mandatory middleware overrides for Cloudflare Edge

--- open-next.config.ts
+++ open-next.config.ts
@@ -2,4 +2,12 @@ export default {
   default: {
     minify: true,
   },
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+    },
+  },
 };
